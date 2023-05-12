13853644f11835312f67b5bba847d6684be17a28
Robo Jukić <robo.jukic2@gmail.com>
2023-05-12 17:41:21 +0200

    Delete home images

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,7 @@ function Home() {
     <div className="home-top">
       <div className="container">
         <div className="container-titleAndImg">
-          <img className="home-imgUpDown" src="/bitcoin.png" />
           <h1>TRACK AND TRADE</h1>
-          <img className="home-imgUpDown" src="/etherium.png" />
         </div>
         <h1 className="h1-gradient">CRYPTO CURRENCIES</h1>
         <Featured />
